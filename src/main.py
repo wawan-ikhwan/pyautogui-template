3811d09968630b0c8ea7dd9a37d3b7457baa4b1a
wawan-ikhwan <ikhwanperwira@gmail.com>
2024-01-28 02:43:08 +0700

    add debug on screen for windows

--- src/main.py
+++ src/main.py
@@ -5,11 +5,27 @@ It imports and executes the execute_commands function from the executes module.
 Invoked when the script is run directly. Executes all commands in the commands folder.
 '''
 import platform
+import os
 import logging
+from time import sleep
 from logging import Logger
 import datetime
 from executes import execute_commands_for_windows, execute_commands_for_linux
-import os
+from utilities.windows_debug_on_screen import WindowsDebugScreen
+from collections import deque
+
+class LabelUpdateHandler(logging.Handler):
+  def __init__(self, label):
+    super().__init__()
+    self.label = label
+    self.last_logs = deque(maxlen=10)
+
+  def emit(self, record):
+    log_message: str = self.format(record)
+    self.last_logs.append(log_message)
+
+    # Update the label with the log messages, separated by new lines
+    self.label.update_label('\n'.join(self.last_logs))
 
 if __name__ == "__main__":
   current_datetime: datetime = datetime.datetime.now()
@@ -17,16 +33,23 @@ if __name__ == "__main__":
   logging.basicConfig(
     filename=f"{current_directory_of_this_python_file}/../log/bot_{current_datetime.strftime('%Y-%m-%d_%H-%M-%S')}.log",
     level=logging.DEBUG,
-    format='%(asctime)s %(levelname)s %(name)s %(message)s'
+    format='%(asctime)s %(levelname)s %(name)s %(message)s',
   )
   logger: Logger = logging.getLogger(__name__)
   current_platform: str = platform.system()
   logger.info('Application started with current platform: %s', current_platform)
   if current_platform == 'Windows':
+    windows_label = WindowsDebugScreen()
+    # Create the custom handler and add it to the root logger
+    label_update_handler = LabelUpdateHandler(windows_label)
+    logging.getLogger().addHandler(label_update_handler)
     execute_commands_for_windows()
+    logger.info('This app close in 5 seconds...')
+    sleep(5)
   elif current_platform == 'Linux':
     execute_commands_for_linux()
   else:
     logger.critical('Current platform (%s) not supported.', current_platform)
     raise Exception(f'Platform {current_platform} not supported.')
   logger.info('Application finished with total elapsed time: %s', str(datetime.datetime.now() - current_datetime))
+  sleep(5)
---
+++ src/utilities/windows_debug_on_screen.py
@@ -0,0 +1,34 @@
+import tkinter as Tkinter
+import win32api, win32con, pywintypes
+
+class WindowsDebugScreen:
+  def __init__(self):
+    label = Tkinter.Label(text='Debug', font=('Consolas', 12), fg='lime', bg='black', justify=Tkinter.LEFT)
+    label.master.overrideredirect(True)
+    label.master.geometry("+20+20")
+    label.master.lift()
+    label.master.wm_attributes("-topmost", True)
+    label.master.wm_attributes("-disabled", True)
+    label.master.wm_attributes("-transparentcolor", "black")
+
+    hWindow = pywintypes.HANDLE(int(label.master.frame(), 16))
+    exStyle: int = win32con.WS_EX_COMPOSITED | win32con.WS_EX_LAYERED | win32con.WS_EX_NOACTIVATE | win32con.WS_EX_TOPMOST | win32con.WS_EX_TRANSPARENT
+    win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
+
+    label.pack()
+    self.label = label
+
+  def hide_label(self):
+    global windows_label
+    self.label.master.withdraw()
+    self.label.master.update()
+
+  def show_label(self):
+    global windows_label
+    self.label.master.deiconify()
+    self.label.master.update()
+
+  def update_label(self, new_text):
+    global windows_label
+    self.label.config(text=new_text)
+    self.label.master.update()
