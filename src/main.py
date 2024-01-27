16291862e9c9581472366ccd05d41e509ea5d68f
wawan-ikhwan <ikhwanperwira@gmail.com>
2024-01-27 23:56:38 +0700

    separating platform in main.py instead of executes.py

--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ It imports and executes the execute_commands function from the executes module.
 
 Invoked when the script is run directly. Executes all commands in the commands folder.
 '''
+import platform
 import logging
+from logging import Logger
 import datetime
-from executes import execute_commands
+from executes import execute_commands_for_windows, execute_commands_for_linux
 
 if __name__ == "__main__":
   current_datetime: datetime = datetime.datetime.now()
@@ -15,6 +17,14 @@ if __name__ == "__main__":
     level=logging.DEBUG,
     format='%(asctime)s %(levelname)s %(name)s %(message)s'
   )
-  logging.info('Starting application...')
-  execute_commands()
-  logging.info('Application finished with total elapsed time: %s', str(datetime.datetime.now() - current_datetime))
+  logger: Logger = logging.getLogger(__name__)
+  current_platform: str = platform.system()
+  logger.info('Application started with current platform: %s', current_platform)
+  if current_platform == 'Windows':
+    execute_commands_for_windows()
+  elif current_platform == 'Linux':
+    execute_commands_for_linux()
+  else:
+    logger.error('Platform not supported.')
+    raise Exception('Platform not supported.')
+  logger.info('Application finished with total elapsed time: %s', str(datetime.datetime.now() - current_datetime))
