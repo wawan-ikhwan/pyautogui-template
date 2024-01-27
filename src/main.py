c3fa9c5db70fd970d3e1105892785a319239461d
wawan-ikhwan <ikhwanperwira@gmail.com>
2024-01-28 03:32:59 +0700

    pylint rules refactor

--- src/main.py
+++ src/main.py
@@ -17,6 +17,7 @@ from utilities.log_handler import LabelUpdateHandler
 if __name__ == "__main__":
   current_datetime: datetime = datetime.datetime.now()
   current_directory_of_this_python_file: str = os.path.dirname(os.path.realpath(__file__))
+  #pylint: disable=line-too-long
   logging.basicConfig(
     filename=f"{current_directory_of_this_python_file}/../log/bot_{current_datetime.strftime('%Y-%m-%d_%H-%M-%S')}.log",
     level=logging.DEBUG,
@@ -26,12 +27,16 @@ if __name__ == "__main__":
   current_platform: str = platform.system()
   logger.info('Application started with current platform: %s', current_platform)
   if current_platform == 'Windows':
-    logging.getLogger().addHandler(LabelUpdateHandler(WINDOWS_LABEL)) # Create the custom handler and add it to the root logger
+    logging.getLogger().addHandler(
+      LabelUpdateHandler(WINDOWS_LABEL)
+    ) # Create the custom handler and add it to the root logger
     execute_commands_for_windows()
   elif current_platform == 'Linux':
     execute_commands_for_linux()
   else:
     logger.critical('Current platform (%s) not supported.', current_platform)
     raise Exception(f'Platform {current_platform} not supported.')
-  logger.info('Application finished with total elapsed time: %s', str(datetime.datetime.now() - current_datetime))
+  logger.info('Application finished with total elapsed time: %s',
+    str(datetime.datetime.now() - current_datetime)
+  )
   sleep(10) # Just to display last log
